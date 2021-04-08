08665da285766b2dabb71e772ed512f5d2a8feb8
Ruby Lazuli <nipkraps@gmail.com>
2021-04-07 21:44:33 -0500

    models: remove Characters::with_name_for_user, silence lint for with_id

--- src/models/characters.rs
+++ src/models/characters.rs
@@ -11,6 +11,8 @@ pub struct Character {
 }
 
 impl Character {
+    // For future additions
+    #[allow(dead_code)]
     /// Returns information about the character with the given id in the
     /// database or an error if something went wrong.
     pub async fn with_id(char_id: u32) -> Result<Self, ModelError> {
@@ -18,25 +20,6 @@ impl Character {
             Character,
             "select * from characters where char_id = $1 limit 1",
             char_id
-        )
-        .fetch_one(&*DB_POOL)
-        .await?;
-
-        Ok(res)
-    }
-
-    /// Returns the character with the given name for this user
-    /// or an error if something went wrong.
-    pub async fn with_name_for_user(name: &str, user_id: u64) -> Result<Self, ModelError> {
-        // Cast done here since this is an implementation detail.
-        // sqlite does not accept `u64`s.
-        let user_id = user_id as i64;
-
-        let res = sqlx::query_as!(
-            Character,
-            "select * from characters where (char_name = $1) and (user_id = $2) limit 1",
-            name,
-            user_id
         )
         .fetch_one(&*DB_POOL)
         .await?;
